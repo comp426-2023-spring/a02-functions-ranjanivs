4e63702eb999f8cb214e5a64ae597befa1a97007
Ranjani Venkatesh <ranjani@email.unc.edu>
2023-03-12 20:18:24 -0400

    adding more response text

--- cli.js
+++ cli.js
@@ -30,15 +30,42 @@ if(argv.z != null){
 // latitude and longitude variables
 let latitude, longitude;
 
-if(argv.n) {
+if(argv.n && argv.n >= 0) {
     latitude = argv.n
-} else if (argv.s) {
+} else if (argv.s && argv.s < 0) {
     latitude = argv.s*-1
+} else {
+    console.log("Latitude out of range.");
+    process.exit(0);
 }
 
-if(argv.e) {
+if(argv.e && argv.e >=0) {
     longitude = argv.e
-} else if(argv.w) {
+} else if(argv.w && argv.w < 0) {
     longitude = argv.w*-1
+} else {
+    console.log("Longitude out of range");
+    process.exit(0);
 }
  
+// Make a request
+const response = await fetch('https://api.open-meteo.com/v1/forecast?latitude=' +latitude+'&longitude='+longitude+'&daily=precipitation_hours&timezone='+timezone);
+
+// Get the data from the request
+const data = await response.json();
+
+//create response text
+const days = args.d ?? 1
+
+if(argv.j) {
+    console.log(data);
+    process.exit(0);
+}
+
+if (days == 0) {
+  console.log("today.")
+} else if (days > 1) {
+  console.log("in " + days + " days.")
+} else {
+  console.log("tomorrow.")
+}
